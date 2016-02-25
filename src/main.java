3c2d6e4c93e759359b38da3c586656d173009271
Cihan-Tiambeng <cihan.tiambeng@gmail.com>
2016-02-25 22:47:12 +0200

    Added files via upload

--- src/main.java
+++ src/main.java
@@ -2,21 +2,46 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class main {
-	
-	public static void main(String [ ] args)
-	{
+	public static void main(String [ ] args){
 		Random randomGenerator = new Random();
-		int randomInt = randomGenerator.nextInt(100); //0..99
+		int agenda.remove(randomInt); = 1;
+		int i = 0;
 		
-	}
-	
-	public void checkVisited(ArrayList<treeNode> extendedList){
-		// check the iterator shits
-		// compare and shit
-		for (int i = 0; i < extendedList.size(); i++)
-		{
+		ArrayList<treeNode> path = new ArrayList<treeNode>();
+		ArrayList<ArrayList<treeNode>> agenda = new <ArrayList<treeNode> >();
+		ArrayList<treeNode> addList = new ArrayList<treeNode>();
+		ArrayList<treeNode> addPath = new ArrayList<treeNode>();
+		treeNode head = new treeNode();
+		
+		agenda.add(head);
+		
+		
+		while( agenda.get(randomInt).isVisited() == false ){
+			i = path.size() - 1;
 			
+			if( agenda.get(randomInt).isVisited ){
+				// do nothing, can swtich condition
+			}
+			else{ // if not visited expand it
+				
+				addList = path.get(i).expand();
+				addPath = path.clone(); 
+				addPath.add(addList.get(0));
+			
+				for (int n = 0; n <= 4; i++){
+					addPath = path.clone(); 
+					addPath.add(addList.get(i));
+					agenda.add(addPath);
+				}
+				agenda.remove(randomInt);
+			}
+			
+			//add to extended list
+			agenda.remove(randomInt);
+			randomInt = randomGenerator.nextInt(agenda.size() - 1) + 1; // so it can never be 0
+			agenda.get(randomInt); // move onto next one
 		}
+		
 		
 	}
 }
